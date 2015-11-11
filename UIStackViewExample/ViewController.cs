2cecbea0ef65ab300193f1ccd62bb09158c03ebe
陈贤哲 <cxzllsx@qq.com>
2015-11-11 15:11:49 +0800

    UIStackView

---
+++ UIStackViewExample/ViewController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+using UIKit;
+
+namespace UIStackViewExample
+{
+	public partial class ViewController : UIViewController
+	{
+		public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			distributionSegmentControl.ValueChanged += DistributionSegmentControlValueChanged;
+			alignmentSegmentControl.ValueChanged += AlignmentSegmentControlValueChanged;
+		}
+
+		public void DistributionSegmentControlValueChanged (object sender, EventArgs e)
+		{
+			UIView.Animate (0.5, () => {
+				if (distributionSegmentControl.SelectedSegment == 0) {
+					this.peopleStackView.Distribution = UIStackViewDistribution.Fill;
+				}else if (distributionSegmentControl.SelectedSegment == 1) {
+					this.peopleStackView.Distribution = UIStackViewDistribution.FillEqually;
+				}else if (distributionSegmentControl.SelectedSegment == 2) {
+					this.peopleStackView.Distribution = UIStackViewDistribution.EqualSpacing;
+				}else if (distributionSegmentControl.SelectedSegment == 3) {
+					this.peopleStackView.Distribution = UIStackViewDistribution.EqualCentering;
+				}
+			});
+		}
+
+		public void AlignmentSegmentControlValueChanged (object sender, EventArgs e)
+		{
+			UIView.Animate (0.5, () => {
+				if (alignmentSegmentControl.SelectedSegment == 0) {
+					this.peopleStackView.Alignment = UIStackViewAlignment.Fill;
+				}else if (alignmentSegmentControl.SelectedSegment == 1) {
+					this.peopleStackView.Alignment = UIStackViewAlignment.Top;
+				}else if (alignmentSegmentControl.SelectedSegment == 2) {
+					this.peopleStackView.Alignment = UIStackViewAlignment.Center;
+				}else if (alignmentSegmentControl.SelectedSegment == 3) {
+					this.peopleStackView.Alignment = UIStackViewAlignment.Bottom;
+				}
+			});
+
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
---
+++ UIStackViewExample/ViewController.designer.cs
@@ -0,0 +1,40 @@
+﻿//
+// This file has been generated automatically by MonoDevelop to store outlets and
+// actions made in the Xcode designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using UIKit;
+using Foundation;
+
+namespace UIStackViewExample
+{
+	[Register ("ViewController")]
+	partial class ViewController
+	{	
+		[Outlet]
+		UIStackView peopleStackView{ get; set;}
+
+		[Outlet]
+		UISegmentedControl distributionSegmentControl{ get; set;}
+
+		[Outlet]
+		UISegmentedControl alignmentSegmentControl{ get; set;}
+
+		void ReleaseDesignerOutlets ()
+		{
+			if (peopleStackView != null) {
+				peopleStackView.Dispose ();
+				peopleStackView = null;
+			}
+			if (distributionSegmentControl != null) {
+				distributionSegmentControl.Dispose ();
+				distributionSegmentControl = null;
+			}
+			if (alignmentSegmentControl != null) {
+				alignmentSegmentControl.Dispose ();
+				alignmentSegmentControl = null;
+			}
+
+		}
+	}
+}
